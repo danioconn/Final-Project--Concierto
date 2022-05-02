91b22e900a8933dae425abf0dc3f10e17bd57224
danioconn <oconnoxq@bc.edu>
2022-05-02 18:22:47 -0400

    Final Commit

--- Concierto/StatesViewController.swift
+++ Concierto/StatesViewController.swift
@@ -11,7 +11,7 @@ var userState = ""
 class StatesViewController: UIViewController {
 
     @IBOutlet weak var statePicker: UIPickerView!
-    var states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "Utah", "VT", "VA", "WA", "WV", "WI", "WY"]
+    var states = ["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"]
     override func viewDidLoad() {
         super.viewDidLoad()
         
